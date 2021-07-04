4dbd75a1a96d29c5d50f2a7a0474cd5a5eb95fa5
tiyn <tiyn@mail-mk.eu>
2021-07-04 18:08:26 +0000

    docker: changed to docker-compose

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-docker stop amphora-wiki
-docker rm amphora-wiki
-docker build . -t tiyn/amphora-wiki
-docker run --name amphora-wiki \
-    --restart unless-stopped \
-    -p "5000:5000" \
-    -e FLASK_ENV=development \
-    -d tiyn/amphora-wiki
+docker-compose down
+docker-compose up -d
